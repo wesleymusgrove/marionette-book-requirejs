5a69fb4a425583dcbfeca316c6eae2b8e1db3413
Wesley Musgrove <wesley.p.musgrove@gmail.com>
2013-12-25 22:11:08 -0600

    edit view list controller

--- assets/js/apps/contacts/edit/edit_view.js
+++ assets/js/apps/contacts/edit/edit_view.js
@@ -1,18 +1,21 @@
-/*global ContactManager:true, console:true*/
-ContactManager.module("ContactsApp.Edit", function(Edit, ContactManager, Backbone, Marionette, $, _) {
-  Edit.Contact = ContactManager.ContactsApp.Common.Views.Form.extend({
-    initialize: function() {
-      this.title = "Edit " + this.model.get("firstName") + " ";
-      this.title += this.model.get("lastName");
-    },
+define(["app", "apps/contacts/common/views"], function(ContactManager) {
+  ContactManager.module("ContactsApp.Edit.View", function(View, ContactManager, Backbone, Marionette, $, _) {
+    View.Contact = ContactManager.ContactsApp.Common.Views.Form.extend({
+      initialize: function() {
+        this.title = "Edit " + this.model.get("firstName") + " ";
+        this.title += this.model.get("lastName");
+      },
 
-    onRender: function() {
-      if(this.options.generateTitle) {
-        var $title = $("<h1>", { text: this.title });
-        this.$el.prepend($title);
+      onRender: function() {
+        if(this.options.generateTitle) {
+          var $title = $("<h1>", { text: this.title });
+          this.$el.prepend($title);
+        }
+
+        this.$(".js-submit").text("Update contact");
       }
-
-      this.$(".js-submit").text("Update contact");
-    }
+    });
   });
+
+  return ContactManager.ContactsApp.Edit.View;
 });
